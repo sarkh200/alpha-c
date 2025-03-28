f9c13f8cb586d3aaf6cc2afcd159be8d384ca2e3
Sadra Saharkhiz <sarkh200@gmail.com>
2025-03-28 01:32:02 -0400

    Add files

---
+++ alphac.h
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define skibidi static
+#define floater extern
+#define sus volatile
+#define stan register
+#define typemaxxing typedef
+
+#define npc void
+#define flex auto
+#define lowkey short
+#define highkey long
+
+#define lit int
+#define char
+
+#define blud struct
+#define tea enum
+
+#define yap printf
+#define snatch malloc
+#define ghost free
+
+#define valid true
+#define cap false
+#define no !
+#define L !
+#define ratio <
+#define gucci >
+#define mid ==
+
+#define tiktok while
+#define yeet return
+#define yikes break
+#define cook switch
+#define pov case
+#define cooked default
+#define bet continue
+#define gaslight if
+#define gatekeep else if
+#define girlboss else
+#define serve for
+
+#define epic (
+#define challenge )
+#define chat {
+#define poggers }
+#define pluh ;
+#define sigma int main
---
+++ main.c
@@ -0,0 +1,130 @@
+#include "alphac.h"
+
+npc do_something epic lit x challenge pluh
+npc another_function epic challenge pluh
+
+sigma epic npc challenge
+chat
+	lit num = 10 pluh
+	char chtr = 'a' pluh
+	float fnum = 3.14 pluh
+	double dnum = 9.87 pluh
+
+	serve epic lit i = 0 pluh i ratio num pluh ++i challenge
+	chat
+		yap epic "Iteration: %d \n", i challenge pluh
+	poggers
+
+	gaslight epic chtr mid 'a' challenge
+	chat
+		yap epic "Character is 'a'\n" challenge pluh
+	poggers
+	gatekeep epic chtr mid 'b' challenge
+	chat
+		yap epic "Character is 'b'\n" challenge pluh
+	poggers
+	girlboss
+	chat
+		yap epic "Character is not 'a' or 'b'\n" challenge pluh
+	poggers
+
+	cook epic num challenge
+	chat
+	pov 10:
+		yap epic "Number is 10\n" challenge pluh
+		yikes pluh
+	cooked:
+		yap epic "Number is not 10\n" challenge pluh
+	poggers
+
+	tiktok epic fnum gucci 0.0 challenge
+	chat
+		yap epic "Float number: %.2f\n", fnum challenge pluh
+		fnum -= 0.5 pluh
+	poggers
+
+	lit count = 0 pluh
+	do
+	chat
+		yap epic "Count: %d\n", count challenge pluh
+		count++ pluh
+	poggers tiktok epic count ratio 3 challenge pluh
+
+	do_something epic num challenge pluh
+
+	lit *ptr = &num pluh
+	yap epic "Value of num: %d\n", *ptr challenge pluh
+
+	lit arr[5] = chat 1, 2, 3, 4, 5 poggers pluh
+	yap epic "Array element 2: %d\n", arr[2] challenge pluh
+
+	blud Polit
+	chat
+		lit x pluh
+		lit y pluh
+	poggers pluh
+	blud Polit pt = chat 10, 20 poggers pluh
+	yap epic "Polit coordinates: epic %d, %d challenge\n", pt.x, pt.y challenge pluh
+
+	union Data
+	chat
+		lit i_val pluh
+		float f_val pluh
+		char str[10] pluh
+	poggers pluh
+
+	union Data data pluh
+
+	data.i_val = 100 pluh
+	yap epic "liteger value in union: %d\n", data.i_val challenge pluh
+
+	data.f_val = 20.5 pluh
+	yap epic "Float value in union epic after liteger assignment challenge: %.2f\n", data.f_val challenge pluh
+
+	tea Weekday
+	chat
+		Sun,
+		Mon,
+		Tue,
+		Wed,
+		Thu,
+		Fri,
+		Sat
+	poggers pluh
+	tea Weekday today = Wed pluh
+	yap epic "Today is: %d\n", today challenge pluh
+
+	typemaxxing lit Mylit pluh
+	Mylit age = 30 pluh
+	yap epic "Age: %d\n", age challenge pluh
+
+	sus lit flag = 0 pluh
+
+	stan lit temp = 10 pluh
+
+	auto lit auto_var = 5 pluh
+
+	floater lit ext_var pluh
+
+	skibidi lit skibidi_var = 1 pluh
+	yap epic "skibidi variable epic value persists challenge: %d\n", skibidi_var challenge pluh
+	skibidi_var++ pluh
+
+	yap epic "Size of lit: %zu bytes\n", sizeof epic lit challenge challenge pluh
+
+	lit *snatched = snatch(100) pluh
+
+	ghost(snatched) pluh
+
+	yeet 0 pluh
+poggers
+
+npc do_something epic lit x challenge
+chat
+	yap epic "Inside do_something function, x = %d\n", x challenge pluh
+poggers
+
+npc another_function epic challenge
+chat
+
+poggers
