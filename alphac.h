fbab5c58cbfc04f07399ba6fca0fca4478edb3b4
Sadra Saharkhiz <sarkh200@gmail.com>
2025-03-28 01:32:12 -0400

    add ai slop

--- alphac.h
+++ alphac.h
@@ -49,3 +49,46 @@
 #define poggers }
 #define pluh ;
 #define sigma int main
+
+// Ai generated from here down
+#define vibe unsigned
+#define susmax const
+#define drip static inline
+#define bussin sizeof
+#define slay break
+#define slaps continue
+#define based do
+#define cringe goto
+#define chad typedef
+#define giga volatile
+#define sigma_plus int main epic void challenge
+
+#define dripmax static const
+#define no_cap true
+#define on_god false
+#define ratioed <=
+#define giga_chad >=
+#define wtf ?
+#define bruh :
+#define sus_check assert
+#define flex_tape realloc
+#define npc_return return void
+#define alpha_case default
+#define beta_case case
+
+#define giga_sus volatile const
+#define npc_main int main epic void challenge
+#define litty unsigned int
+#define big_brain double
+#define small_brain float
+#define giga_brain long double
+#define ez_mode 0
+#define hard_mode 1
+#define pro_gamer_move break
+#define npc_move continue
+#define sus_move goto
+#define npc_void void
+#define npc_return_void return
+#define npc_assert assert
+#define npc_malloc malloc
+#define npc_free free
